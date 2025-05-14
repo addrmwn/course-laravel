3fe3b6908c2ac695f88e244bf851e6dda3afb163
addrmwn <adarmawan106@gmail.com>
2025-05-15 02:28:21 +0700

    Course WPU Belajar Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout :title="$title">
+
+    <p>Welcome To my home page</p>
+
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Us']);
+});
